db317c159384a07dbcd6acfe08781fd66166b4ee
Ivan Voronkov <shinodise@gmail.com>
2023-11-25 16:39:53 +0300

    Added line drawing and fill algo

---
+++ Drawing/Filler.cs
@@ -0,0 +1,45 @@
+﻿namespace CG_Lab4.Drawing
+{
+    public class Filler
+    {
+        private int _scaleFactor;
+
+        public Filler(int scaleFactor) 
+        {
+            _scaleFactor = scaleFactor;
+        }
+
+        public List<Point> FloodFill(Bitmap image, Point startPoint, Color fillColor)
+        {
+            var fillPoints = new List<Point>();
+            Color oldColor = image.GetPixel(startPoint.X, startPoint.Y);
+            if (oldColor == fillColor) return fillPoints;
+
+            List<Point> filledPoints = new List<Point>();
+            Stack<Point> stack = new Stack<Point>();
+            stack.Push(startPoint);
+
+            while (stack.Count > 0)
+            {
+                Point point = stack.Pop();
+                int x = point.X;
+                int y = point.Y;
+
+                if (x < 0 || x >= image.Width || y < 0 || y >= image.Height)
+                    continue;
+
+                if (image.GetPixel(x * _scaleFactor, y * _scaleFactor) == oldColor)
+                {
+                    filledPoints.Add(new Point(x, y));
+
+                    stack.Push(new Point(x + 1, y));
+                    stack.Push(new Point(x - 1, y));
+                    stack.Push(new Point(x, y + 1));
+                    stack.Push(new Point(x, y - 1));
+                }
+            }
+
+            return fillPoints;
+        }
+    }
+}
---
+++ Drawing/LineDrawer.cs
@@ -0,0 +1,53 @@
+﻿namespace CG_Lab4.Drawing
+{
+    public class LineDrawer
+    {
+        private int _scaleFactor;
+
+        public LineDrawer(int scaleFactor)
+        {
+            _scaleFactor = scaleFactor;
+        }
+
+        public List<Point> DrawLine(Point point1, Point point2)
+        {
+            List<Point> linePoints = new List<Point>();
+
+            int x1 = point1.X;
+            int y1 = point1.Y;
+            int x2 = point2.X;
+            int y2 = point2.Y;
+
+            int dx = Math.Abs(x2 - x1);
+            int dy = Math.Abs(y2 - y1);
+            int sx = (x1 < x2) ? 1 : -1;
+            int sy = (y1 < y2) ? 1 : -1;
+
+            int err = dx - dy;
+
+            while (true)
+            {
+                linePoints.Add(new Point(x1 * _scaleFactor, y1 * _scaleFactor));
+
+                if (x1 == x2 && y1 == y2)
+                    break;
+
+                int err2 = 2 * err;
+
+                if (err2 > -dy)
+                {
+                    err -= dy;
+                    x1 += sx;
+                }
+
+                if (err2 < dx)
+                {
+                    err += dx;
+                    y1 += sy;
+                }
+            }
+
+            return linePoints;
+        }
+    }
+}
