f382f769452bfd6509cb43af2a6a8173399f5b80
Ivan Voronkov <shinodise@gmail.com>
2023-11-28 16:42:02 +0300

    Added clipping algorithms

---
+++ Drawing/CohenSutherland.cs
@@ -0,0 +1,54 @@
+﻿using CG_Lab4.Models;
+using Point = CG_Lab4.Models.Point;
+using Rectangle = CG_Lab4.Models.Rectangle;
+
+namespace CG_Lab4.Drawing
+{
+    public class CohenSutherland
+    {
+        // Коды для каждого положения точки относительно окна
+        private const int INSIDE = 0; // 0000
+        private const int LEFT = 1;   // 0001
+        private const int RIGHT = 2;  // 0010
+        private const int BOTTOM = 4; // 0100
+        private const int TOP = 8;    // 1000
+
+        public static void ClipToRectangle(Triangle triangle, Rectangle rect)
+        {
+            for (int i = 0; i < triangle.Points.Count; i++)
+            {
+                // TODO: проверить правильность передачи точек
+                ClipPoint(triangle.Points, i, rect.Points[3].X, rect.Points[1].Y, rect.Points[1].X, rect.Points[3].Y);
+            }
+        }
+
+        private static void ClipPoint(List<Point> vertices, int index, int xmin, int ymin, int xmax, int ymax)
+        {
+            int code = INSIDE;
+            Point p = vertices[index];
+
+            if (p.X < xmin)
+                code |= LEFT;
+            else if (p.X > xmax)
+                code |= RIGHT;
+
+            if (p.Y < ymin)
+                code |= BOTTOM;
+            else if (p.Y > ymax)
+                code |= TOP;
+
+            switch (code)
+            {
+                case INSIDE:
+                    break;
+                default:
+                    vertices[index] = new Point
+                    {
+                        X = Math.Clamp(p.X, xmin, xmax),
+                        Y = Math.Clamp(p.Y, ymin, ymax)
+                    };
+                    break;
+            }
+        }
+    }
+}
---
+++ Drawing/SutherlandHodgman.cs
@@ -0,0 +1,80 @@
+﻿using Point = CG_Lab4.Models.Point;
+
+namespace CG_Lab4.Drawing
+{
+    public class SutherlandHodgman
+    {
+        public static List<Point> ClipPolygon(List<Point> subjectPolygon, List<Point> clipPolygon)
+        {
+            List<Point> clippedPolygon = new List<Point>(subjectPolygon);
+
+            for (int i = 0; i < clipPolygon.Count; i++)
+            {
+                int nextIndex = (i + 1) % clipPolygon.Count;
+                Point clipEdgeStart = clipPolygon[i];
+                Point clipEdgeEnd = clipPolygon[nextIndex];
+
+                clippedPolygon = ClipEdge(clippedPolygon, clipEdgeStart.X, clipEdgeStart.Y, clipEdgeEnd.X, clipEdgeEnd.Y);
+            }
+
+            return clippedPolygon;
+        }
+
+        private static List<Point> ClipEdge(List<Point> polygon, int x1, int y1, int x2, int y2)
+        {
+            List<Point> clippedPolygon = new List<Point>();
+
+            for (int i = 0; i < polygon.Count; i++)
+            {
+                Point current = polygon[i];
+                Point next = polygon[(i + 1) % polygon.Count];
+
+                if (IsInside(current.X, current.Y, x1, y1, x2, y2))
+                {
+                    if (!IsInside(next.X, next.Y, x1, y1, x2, y2))
+                    {
+                        Point intersection = GetIntersection(current, next, x1, y1, x2, y2);
+                        clippedPolygon.Add(intersection);
+                    }
+
+                    clippedPolygon.Add(next);
+                }
+                else if (IsInside(next.X, next.Y, x1, y1, x2, y2))
+                {
+                    Point intersection = GetIntersection(current, next, x1, y1, x2, y2);
+                    clippedPolygon.Add(intersection);
+                }
+            }
+
+            return clippedPolygon;
+        }
+
+        private static bool IsInside(int x, int y, int x1, int y1, int x2, int y2)
+        {
+            return (x - x1) * (y2 - y1) - (y - y1) * (x2 - x1) >= 0;
+        }
+
+        private static Point GetIntersection(Point p1, Point p2, int x1, int y1, int x2, int y2)
+        {
+            int dx1 = p2.X - p1.X;
+            int dy1 = p2.Y - p1.Y;
+
+            int dx2 = x2 - x1;
+            int dy2 = y2 - y1;
+
+            int denom = dx1 * dy2 - dy1 * dx2;
+
+            if (denom == 0)
+            {
+                return p1;  // Линии параллельны или совпадают
+            }
+
+            int t = ((x1 - p1.X) * dy2 - (y1 - p1.Y) * dx2) / denom;
+
+            int ix = p1.X + t * dx1;
+            int iy = p1.Y + t * dy1;
+
+            return new Point { X = ix, Y = iy };
+        }
+    }
+}
